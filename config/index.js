238cb7e4774e70791c1df87028ac2ae49fdcb8dd
OpenDgK <93925720+OpenDgK@users.noreply.github.com>
2022-09-19 21:35:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oP0hs5kFO-bLOHbgvuDRYY4T1xHs",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "ZyEGmouQAAVCIr7VfDmbPOXrjmqctyIvvD1L70384fY",
+      useTemplateId: "10b5W2464QTwKw23xOjkvfc_dFK2LYqm_dygt7vmis4",
       // 所在省份
       province: "天津",
       // 所在城市
@@ -74,7 +74,7 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '本周',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://www.tjcu.edu.cn/index/jwgl/jwglxt.htm",
+      openUrl: "http://authserver.tjcu.edu.cn/authserver/login?service=http%3A%2F%2Fstu.j.tjcu.edu.cn%2Fstudent%2FteachingResources%2FfreeClassroomQuery%2FtomrrowDate",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -101,7 +101,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oP0hs5k80_xZk3MPskWactpOQDRw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "ZyEGmouQAAVCIr7VfDmbPOXrjmqctyIvvD1L70384fY",
+      useTemplateId: "10b5W2464QTwKw23xOjkvfc_dFK2LYqm_dygt7vmis4",
       // 所在省份
       province: "陕西",
       // 所在城市
